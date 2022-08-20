dbea5e6fa09816b5bf10e65300cbfe1ca5ffdfec
S3597 <mevksneha.3@gmail.com>
2022-08-20 13:15:33 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+    # Program to print the different attributes of my favorite song.
+########################################################################
+
+"""    
+Below are the different attribute values which are assigned to the respective variables.
+"""
+
+FavoriteSong_name = "Willow"         #String value
+Artist = "Taylor Swift"                    #String value
+Album = "Evermore"                              #String value
+Genre = "Chamber folk"                              #String value
+Song_Duration_in_minutes = 3.566            #Float value
+ReleasedYear = 2020                      #Integer value
+
+"""
+Printing all the different attributes of my favorite song
+"""
+
+print(FavoriteSong_name)
+print(Artist)
+print(Album)
+print(Genre)
+print(Song_Duration_in_minutes)
+print(ReleasedYear)
